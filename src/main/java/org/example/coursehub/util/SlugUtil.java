7bd6098bf9ebf0c3e4d114413199b98b59d7455c
Quân <117747610+dnghngqun@users.noreply.github.com>
2025-08-19 19:18:31 +0700

    commit ex

--- src/main/java/org/example/coursehub/util/SlugUtil.java
+++ src/main/java/org/example/coursehub/util/SlugUtil.java
@@ -3,6 +3,7 @@ package org.example.coursehub.util;
 import java.text.Normalizer;
 
 public class SlugUtil {
+    //chuẩn hóa chuỗi thành slug ("Ha Noi 2025!" → "ha-noi-2025-")
     public static String slugify(String input) {
         if (input == null) return null;
         String now = Normalizer.normalize(input, Normalizer.Form.NFD)
